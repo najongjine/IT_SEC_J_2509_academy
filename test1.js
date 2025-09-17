c9e20c84705ff80176569ec50e41e05b7c98b9ce
najongjine <najongjin3@hotmail.com>
2025-09-17 10:27:33 +0900

    .

--- test1.js
+++ test1.js
@@ -4,9 +4,9 @@ let a = 1;
 let b = 2;
 let c = a + b;
 
-a + 3;
+a + 3; //4 가 나오긴 했는데.. 저장을 안함
 console.log(`a:${a}`); // 1
-a = a + 3;
+a = a + 3; // a 의 값과 3을 더한다음, 다시 a에 저장하라
 console.log(`a:${a}`); // 4
 
 
