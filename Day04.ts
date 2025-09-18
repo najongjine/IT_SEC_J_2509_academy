9802c46286d736b7b04387cedfebdc1c5d78019d
najongjine <najongjin3@hotmail.com>
2025-09-18 10:37:49 +0900

    .

--- Day04.ts
+++ Day04.ts
@@ -3,6 +3,7 @@
 let a: number = 1;
 let b: string = "2";
 let sum: number = 0;
+
 //sum=a+b;
 /*
 let a=1; 
@@ -24,10 +25,12 @@ let array1: number[] = [1, 2, 3];
 array1[0] = 5;
 array1 = [5];
 console.log(`array1: ${array1}`);
+console.log(`array1[0]: ${array1[0]}`);
 
 let jsObject = { a: 1, b: 2 };
 console.log(`jsObject: `, jsObject);
 console.log(`jsObject a: `, jsObject.a);
+console.log(`jsObject b: `, jsObject.b);
 
 // 지금 이거 해독하지 마세요
 let 내쇼핑몰 = [{ "제품": "멍멍이", "가격": "500" }
