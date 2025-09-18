e663c4b1f550c0887fade98b22fa3fe8840d0024
najongjine <najongjin3@hotmail.com>
2025-09-18 09:45:27 +0900

    .

--- Day04.ts
+++ Day04.ts
@@ -1,5 +1,18 @@
 //node Day04.ts
+// let a=1;
+let a: number = 1;
+let b: string = "2";
+let sum: number = 0;
+//sum=a+b;
+/*
+let a=1; 
+이건 들어오는 값에 따라서 타입이 자동으로 지정이 되요.
 
-let a = 1;
-a = 3;
-console.log(`a: ${a}`);
+let a:number=1;
+이건 내가 직접 타입을 정한 거에요.
+
+let sum:number = 1+"2";
+이런걸 막아줘요
+ */
+
+
