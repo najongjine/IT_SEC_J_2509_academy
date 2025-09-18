973ab300c70d07108053aea0406a62fa10d81b44
najongjine <najongjin3@hotmail.com>
2025-09-18 11:28:45 +0900

    .

--- Day04_if.ts
+++ Day04_if.ts
@@ -5,3 +5,11 @@ if (user_input == door_password) {
 } else {
     console.log(`비밀번호가 틀렸습니다`);
 }
+
+
+//if 문을 쉬운 형태로
+if (true) {
+    console.log("조건이 true 일때만 여기가 실행")
+} else {
+    console.log("조건이 false 일때 실행. 저는 옵션이에요")
+}
